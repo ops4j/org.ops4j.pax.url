58531bd53f9c0a70e5e672c813f17651e33b1577
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-07-13 17:48:29 +0000

    environment should work now
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/url@12287 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-url-mvnlive/src/main/java/org/ops4j/pax/url/mvnlive/internal/Parser.java
+++ pax-url-mvnlive/src/main/java/org/ops4j/pax/url/mvnlive/internal/Parser.java
@@ -1,74 +1,71 @@
 package org.ops4j.pax.url.mvnlive.internal;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 /**
  * @author Toni Menzel (tonit)
  * @since Jul 10, 2008
  */
-public class Parser
-{
+public class Parser {
 
     /**
      * Syntax for the url; to be shown on exception messages.
-     * This accepts groupId/artifact that will mvnlive lead try finding the artifact in the base directory (and sub)
-     *
+     * This accepts groupId/artifact that will mvnlive lead try finding the artifact in the base directory
+     * (which will be calculated ) and below.
+     * <p/>
      * the local_uri should point to a local directory
      */
     private static final String SYNTAX = "mvnlive:groupId/artifactId | mvnlive:local_uri";
     private String m_path;
 
-    public Parser( String path )
-    {
+    public Parser(String path) {
         m_path = path;
     }
 
     /**
      * Will try to find the projectFolder specified in the path.
      *
-     * @return the local projectfolder or null if it is invalid.
+     * @return the local projectfolder
      */
     public File getProjectFolder()
-        throws IOException
-    {
+            throws IOException {
         File res = null;
-        try
-        {
+        try {
             // try to resolve as file path
-            res = new File( new URI( m_path ).getPath() );
+            res = new File(new URI(m_path).getPath());
 
-        } catch( Exception e )
-        {
+        } catch (Exception e) {
             // e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        if( res == null || !res.canRead() )
-        {
+        if (res == null || !res.canRead()) {
             // try to resolve as artifact name:
-            String[] parts = m_path.split( "/" );
+            String[] parts = m_path.split("/");
             // dig up until last pom
-            File last = digUpPomPath( new File( "." ).getCanonicalFile() );
+            File last = digUpPomPath(new File(".").getCanonicalFile());
             // try to find it from current base
-            res = find( last, parts[ 0 ], parts[ 1 ] );
+            res = find(last, parts[0], parts[1]);
 
 
+        }
+        if (res == null) {
+            throw new IllegalArgumentException("Path " + m_path + " is invalid. Project root could not be determined.");
         }
         return res;
     }
 
-    private File digUpPomPath( File file )
-        throws IOException
-    {
-        File deeper = new File( file.getParentFile().getCanonicalPath() + "/pom.xml" );
+    private File digUpPomPath(File file)
+            throws IOException {
+        File deeper = new File(file.getParentFile().getCanonicalPath() + "/pom.xml");
 
-        if( deeper.exists() && deeper.canRead() )
-        {
-            return digUpPomPath( deeper.getParentFile() );
+        if (deeper.exists() && deeper.canRead()) {
+            return digUpPomPath(deeper.getParentFile());
         }
         // return current file
         //    System.out.println( "Project base is " + file.getCanonicalPath() );
@@ -82,23 +79,16 @@ public class Parser
      * @param groupId
      * @param artifactId
      */
-    private File find( File curr, String groupId, String artifactId )
-    {
+    private File find(File curr, String groupId, String artifactId) {
         // asume that we should have to walk down the base dir
-        for( File c : curr.listFiles() )
-        {
-            if( c.isFile() && c.getName().equals( "pom.xml" ) )
-            {
-                if( isMatchingPom( c, groupId, artifactId ) )
-                {
+        for (File c : curr.listFiles()) {
+            if (c.isFile() && c.getName().equals("pom.xml")) {
+                if (isMatchingPom(c, groupId, artifactId)) {
                     return c.getParentFile();
                 }
-            }
-            else if( c.isDirectory() && !"src,target,site".contains( c.getName() ) )
-            {
-                File r = find( c, groupId, artifactId );
-                if( r != null )
-                {
+            } else if (c.isDirectory() && !"src,target,site".contains(c.getName())) {
+                File r = find(c, groupId, artifactId);
+                if (r != null) {
                     return r;
                 }
             }
@@ -108,49 +98,37 @@ public class Parser
     }
 
     // parses the pom
-    private boolean isMatchingPom( File file, String groupId, String artifactId )
-    {
-        if( file.exists() && file.canRead() )
-        {
+    private boolean isMatchingPom(File file, String groupId, String artifactId) {
+        if (file.exists() && file.canRead()) {
             String foundGroup = null;
             String foundArtifact = null;
-            try
-            {
+            try {
                 XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                factory.setNamespaceAware( true );
+                factory.setNamespaceAware(true);
                 XmlPullParser xpp = factory.newPullParser();
-                xpp.setInput( new FileInputStream( file ), null );
+                xpp.setInput(new FileInputStream(file), null);
                 // parse the flat pom stuff out
                 int eventType = xpp.getEventType();
-                while( eventType != XmlPullParser.END_DOCUMENT )
-                {
-                    if( eventType == XmlPullParser.START_TAG && xpp.getDepth() == 2 )
-                    {
-                        if( xpp.getName().equals( "groupId" ) )
-                        {
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG && xpp.getDepth() == 2) {
+                        if (xpp.getName().equals("groupId")) {
                             xpp.next();
                             foundGroup = xpp.getText();
-                        }
-                        else if( xpp.getName().equals( "artifactId" ) )
-                        {
+                        } else if (xpp.getName().equals("artifactId")) {
                             xpp.next();
                             foundArtifact = xpp.getText();
                         }
                     }
                     eventType = xpp.next();
                 }
-            } catch( XmlPullParserException e )
-            {
+            } catch (XmlPullParserException e) {
                 e.printStackTrace();
-            } catch( IOException e )
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            return ( ( groupId.equals( foundGroup ) && ( artifactId.equals( foundArtifact ) ) ) );
-        }
-        else
-        {
+            return ((groupId.equals(foundGroup) && (artifactId.equals(foundArtifact))));
+        } else {
             return false;
         }
 
--- pax-url-mvnlive/src/test/java/org/ops4j/pax/url/mvnlive/ConnectionTest.java
+++ pax-url-mvnlive/src/test/java/org/ops4j/pax/url/mvnlive/ConnectionTest.java
@@ -1,80 +1,71 @@
 package org.ops4j.pax.url.mvnlive;
 
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
 import org.junit.After;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-import org.osgi.service.url.AbstractURLStreamHandlerService;
 import org.ops4j.pax.runner.handler.internal.URLStreamHandlerExtender;
 import org.ops4j.pax.url.mvnlive.internal.Configuration;
 import org.ops4j.pax.url.mvnlive.internal.ConfigurationImpl;
 import org.ops4j.pax.url.mvnlive.internal.Connection;
 import org.ops4j.util.property.PropertyResolver;
+import org.osgi.service.url.AbstractURLStreamHandlerService;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author Toni Menzel (tonit)
  * @since Jul 10, 2008
  */
-public class ConnectionTest
-{
+public class ConnectionTest {
 
     private URLStreamHandlerExtender m_extender;
 
     //@Test
     public void connectionFormatFullLocalPath()
-        throws IOException
-    {
+            throws IOException {
 
         // this will trigger anther project to build.
         Connection conn =
-            new Connection( new URL( "mvnlive:file:///Users/tonit/devel/pax/url/pax-url-bnd" ), new Configuration()
-            {
+                new Connection(new URL("mvnlive:file:///Users/tonit/devel/pax/url/pax-url-bnd"), new Configuration() {
 
-            }
-            );
-        assertTrue( conn.getInputStream().available() > 0 );
+                }
+                );
+        assertTrue(conn.getInputStream().available() > 0);
 
     }
 
     @Test
     public void conectionFormatArtifact()
-        throws IOException
-    {
+            throws IOException {
 
         // this will trigger anther project to build.
         Connection conn =
-            new Connection( new URL( "mvnlive:org.ops4j.pax.url/pax-url-bnd" ), new Configuration()
-            {
+                new Connection(new URL("mvnlive:org.ops4j.pax.url/pax-url-bnd"), new Configuration() {
 
-            }
-            );
+                }
+                );
 
-        //assertTrue( conn.getInputStream().available() > 0 );
+        assertTrue(conn.getInputStream().available() > 0);
 
     }
 
     @Before
-    public void setup()
-    {
+    public void setup() {
         // add m2 home:
-        System.setProperty( "M2_HOME","/Users/tonit/devel/maven" );
+        //System.setProperty( "M2_HOME","/Users/tonit/devel/maven" );
 
         // add handler, which can be scipped if using paxdrone with paxrunner connector later.
         m_extender = new URLStreamHandlerExtender();
-        m_extender.register( new String[]{ "mvnlive" }, new AbstractURLStreamHandlerService()
-        {
+        m_extender.register(new String[]{"mvnlive"}, new AbstractURLStreamHandlerService() {
 
-            public URLConnection openConnection( URL url )
-                throws IOException
-            {
-                return new Connection( url, new ConfigurationImpl( new PropertyResolver()
-                {
+            public URLConnection openConnection(URL url)
+                    throws IOException {
+                return new Connection(url, new ConfigurationImpl(new PropertyResolver() {
 
-                    public String get( String s )
-                    {
+                    public String get(String s) {
                         return null;
                     }
                 }
@@ -87,8 +78,7 @@ public class ConnectionTest
     }
 
     @After
-    public void end()
-    {
-        m_extender.unregister( new String[]{ "mvnlive" } );
+    public void end() {
+        m_extender.unregister(new String[]{"mvnlive"});
     }
 }
