39490ec619c1eedb845336ed15fa43a8a73fd266
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-12 13:32:02 +0000

    PAXURL-38 Make "wrap:" protocol usable in non osgi environments
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/url@14489 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-url-link/src/main/java/org/ops4j/pax/url/link/Handler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2009 Alin Dreghiciu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.url.link;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+/**
+ * TODO Add JavaDoc.
+ *
+ * @author Alin Dreghiciu (adreghiciu@gmail.com)
+ * @since 0.5.0, March 12, 2009
+ */
+public class Handler
+    extends URLStreamHandler
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected URLConnection openConnection( final URL url )
+        throws IOException
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+    
+}
