6b63083a7173bb9ef4d8e43d173d6f1ecb405cbc
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-22 20:06:37 +0000

    PAXURL-43 Implement maven: protocol (Mercury based)
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/url@14593 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-url-maven/src/main/java/org/ops4j/pax/url/maven/internal/Connection.java
+++ pax-url-maven/src/main/java/org/ops4j/pax/url/maven/internal/Connection.java
@@ -17,15 +17,37 @@
  */
 package org.ops4j.pax.url.maven.internal;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.maven.mercury.artifact.Artifact;
+import org.apache.maven.mercury.artifact.ArtifactBasicMetadata;
+import org.apache.maven.mercury.artifact.ArtifactMetadata;
+import org.apache.maven.mercury.artifact.Quality;
+import org.apache.maven.mercury.artifact.QualityRange;
+import org.apache.maven.mercury.builder.api.DependencyProcessor;
+import org.apache.maven.mercury.repository.api.ArtifactBasicResults;
+import org.apache.maven.mercury.repository.api.ArtifactResults;
+import org.apache.maven.mercury.repository.api.Repository;
+import org.apache.maven.mercury.repository.api.RepositoryException;
+import org.apache.maven.mercury.repository.local.m2.LocalRepositoryM2;
+import org.apache.maven.mercury.repository.remote.m2.RemoteRepositoryM2;
+import org.apache.maven.mercury.repository.virtual.VirtualRepositoryReader;
+import org.apache.maven.mercury.transport.api.Server;
 import org.ops4j.lang.NullArgumentException;
 import org.ops4j.pax.url.maven.commons.MavenConfiguration;
+import org.ops4j.pax.url.maven.commons.MavenRepositoryURL;
 
 /**
  * An URLConnextion that supports maven: protocol.
@@ -45,6 +67,14 @@ public class Connection
      * Service configuration.
      */
     private final MavenConfiguration m_configuration;
+    /**
+     * Parsed url.
+     */
+    private Parser m_parser;
+    /**
+     * Mercury virtual repository reader.
+     */
+    private VirtualRepositoryReader m_vrr;
 
     /**
      * Creates a new connection.
@@ -61,6 +91,7 @@ public class Connection
         NullArgumentException.validateNotNull( url, "URL cannot be null" );
         NullArgumentException.validateNotNull( configuration, "Service configuration" );
         m_configuration = configuration;
+        m_parser = new Parser( url.getPath() );
     }
 
     /**
@@ -68,8 +99,52 @@ public class Connection
      */
     @Override
     public void connect()
+        throws IOException
     {
-        // do nothing
+        final List<Repository> repositories = new ArrayList<Repository>();
+        final MavenRepositoryURL localRepository = m_configuration.getLocalRepository();
+        LOG.debug( "Using local repository " + localRepository );
+        if( localRepository != null )
+        {
+            try
+            {
+                repositories.add(
+                    new LocalRepositoryM2(
+                        "localRepo",
+                        new File( localRepository.toURL().toURI() ),
+                        DependencyProcessor.NULL_PROCESSOR
+                    )
+                );
+            } catch( URISyntaxException e )
+            {
+                throw initIOException( "Cannot initialize Maven local repository", e );
+            }
+        }
+        final List<MavenRepositoryURL> remoteRepos = new ArrayList<MavenRepositoryURL>();
+        if( m_parser.getRepositoryURL() != null )
+        {
+            remoteRepos.add( 0, m_parser.getRepositoryURL() );
+        }
+        remoteRepos.addAll( m_configuration.getRepositories() );
+        for( MavenRepositoryURL repositoryURL : remoteRepos )
+        {
+            LOG.debug( "Using remote repository " + repositoryURL );
+            final Server server = new Server( repositoryURL.toURL().toExternalForm(), repositoryURL.toURL() );
+            final RemoteRepositoryM2 remoteRepo =
+                new RemoteRepositoryM2( server.getId(), server, DependencyProcessor.NULL_PROCESSOR );
+            remoteRepo.setRepositoryQualityRange(
+                createQualityRange( repositoryURL.isReleasesEnabled(), repositoryURL.isSnapshotsEnabled() )
+            );
+            repositories.add( remoteRepo );
+        }
+        try
+        {
+            m_vrr = new VirtualRepositoryReader( repositories );
+        }
+        catch( RepositoryException e )
+        {
+            throw initIOException( "Cannot configure Maven repositories", e );
+        }
     }
 
     /**
@@ -80,12 +155,112 @@ public class Connection
         throws IOException
     {
         connect();
+
         LOG.debug( "Resolving [" + url.toExternalForm() + "]" );
+        try
+        {
+            List<ArtifactBasicMetadata> query = new ArrayList<ArtifactBasicMetadata>();
+            final ArtifactMetadata queryMeta = new ArtifactMetadata( m_parser.getGAV() );
+            query.add( queryMeta );
+
+            final ArtifactBasicResults results = m_vrr.readVersions( query );
+            if( results == null )
+            {
+                throw new IOException( "Cannot determine artifact versions from [" + m_parser.getGAV() );
+            }
+            if( results.hasExceptions() || !results.hasResults( queryMeta ) )
+            {
+                //noinspection ThrowableResultOfMethodCallIgnored
+                throw initIOException( "Cannot determine artifact versions", results.getError( queryMeta ) );
+            }
+
+            final List<ArtifactBasicMetadata> foundArtifacts = results.getResult( queryMeta );
+            if( LOG.isDebugEnabled() )
+            {
+                for( ArtifactBasicMetadata foundArtifact : foundArtifacts )
+                {
+                    LOG.debug( "Found artifact [" + foundArtifact.getGAV() + "]" );
+                }
+
+                // download the artifact
+                // TODO selecting the last artifact in the list is not the correct approach
+                final ArtifactBasicMetadata foundArtifact = foundArtifacts.get( foundArtifacts.size() - 1 );
+
+                LOG.debug( "Selected artifact [" + foundArtifact.getGAV() + "]" );
+
+                final ArtifactResults artifactResults = m_vrr.readArtifacts( toList( foundArtifact ) );
+                if( artifactResults.hasExceptions() || !artifactResults.hasResults( foundArtifact ) )
+                {
+                    //noinspection ThrowableResultOfMethodCallIgnored
+                    throw initIOException( "Cannot download", artifactResults.getError( foundArtifact ) );
+                }
+                final List<Artifact> artifacts = artifactResults.getResults( foundArtifact );
+                final File artifactFile = artifacts.get( 0 ).getFile();
+                if( artifactFile == null )
+                {
+                    throw new IOException( "Cannot download artifact [" + foundArtifact.getGAV() );
+                }
+                return new BufferedInputStream( new FileInputStream( artifactFile ) );
+            }
+        }
+        catch( RepositoryException e )
+        {
+            throw initIOException( "Canot determine artifacts versions", e );
+        }
 
         // no artifact found
         throw new RuntimeException(
             "URL [" + url.toExternalForm() + "] could not be resolved. (enable TRACE logging for details)"
         );
+    }
+
+    //TODO remove once a version > 1.0-alpha-5 of Mercury and use QualityRange.create
+    private static QualityRange createQualityRange( boolean releases, boolean snapshots )
+    {
+        if( releases && snapshots )
+        {
+            return QualityRange.ALL;
+        }
+        else if( releases )
+        {
+            return new QualityRange( Quality.ALPHA_QUALITY, true, Quality.RELEASE_QUALITY, true );
+        }
+        else if( snapshots )
+        {
+            return new QualityRange( Quality.SNAPSHOT_QUALITY, true, Quality.SNAPSHOT_TS_QUALITY, true );
+        }
+
+        throw new IllegalArgumentException( "Unsuported combination for releases/snapshots" );
+    }
+
+    /**
+     * Creates an IOException with a message and a cause.
+     *
+     * @param message exception message
+     * @param cause   exception cause
+     *
+     * @return the created IO Exception
+     */
+    private IOException initIOException( final String message, final Exception cause )
+    {
+        IOException exception = new IOException( message );
+        exception.initCause( cause );
+        return exception;
+    }
+
+    /**
+     * Utility to add var args elements into a list.
+     *
+     * @param elements to add
+     * @param <T>      any type
+     *
+     * @return list
+     */
+    private <T> List<T> toList( final T... elements )
+    {
+        final ArrayList<T> list = new ArrayList<T>();
+        list.addAll( Arrays.asList( elements ) );
+        return list;
     }
 
 }
