d05c1f6603a74c1b8793a6debda7eea13be5c0c3
ANierbeck <bcanhome@googlemail.com>
2011-01-27 21:00:12 +0100

    [PAXURL-100] - Improve - Import-Package: OSGi Spec states that javax.servlet and javax.servlet.http are mandatory

--- pax-url-war/src/main/java/org/ops4j/pax/url/war/internal/WarConnection.java
+++ pax-url-war/src/main/java/org/ops4j/pax/url/war/internal/WarConnection.java
@@ -34,7 +34,10 @@ public class WarConnection
     extends AbstractConnection
 {
 
-    /**
+    private static final String JAVAX_SERVLET_HTTP = "javax.servlet.http";
+	private static final String JAVAX_SERVLET = "javax.servlet";
+
+	/**
      * @see AbstractConnection#AbstractConnection(URL, Configuration)
      */
     public WarConnection( final URL url,
@@ -63,19 +66,6 @@ public class WarConnection
                 + "javax.*; resolution:=optional,"
                 + "org.xml.*; resolution:=optional,"
                 + "org.w3c.*; resolution:=optional";
-            if( getConfiguration().getImportPaxLoggingPackages() )
-            {
-                String provider = ";provider=paxlogging;resolution:=optional";
-                packages +=
-                        ",org.apache.commons.logging" + provider +
-                        ",org.apache.commons.logging.impl" + provider +
-                        ",org.apache.log4j" + provider +
-                        ",org.apache.log4j.spi" + provider +
-                        ",org.apache.log4j.xml" + provider +
-                        ",org.slf4j" + provider +
-                        ",org.slf4j.helpers" + provider +
-                        ",org.slf4j.spi" + provider;
-            }
             instructions.setProperty(
                 "Import-Package",
                 packages
@@ -83,12 +73,55 @@ public class WarConnection
         } else {
         	// Certain Packages are always needed and therefore should be appended.
         	String importPackages = instructions.getProperty("Import-Package");
-        	if (importPackages.indexOf("javax.servlet") == -1) { //TODO better regex
-        		importPackages +=",javax.servlet";
-        	} else if (importPackages.indexOf("javax.servlet.http") == -1) {
-        		importPackages += ",javax.servlet,javax.servlet.http";
+        	
+        	if ((importPackages.contains(JAVAX_SERVLET) )|| (importPackages.contains(JAVAX_SERVLET_HTTP) )) { 
+        		//found javax.servlet
+        		//check if javax.servlet or javax.servlet.http is contained.
+        		boolean servletFound = false;
+        		boolean servletHttpFound = false;
+        		String[] imports = importPackages.split(",");
+        		for (String importstmt : imports) {
+        			if (importstmt.contains(JAVAX_SERVLET)) {
+        				if (importstmt.length() > JAVAX_SERVLET.length() && importstmt.charAt(JAVAX_SERVLET.length()) == '.') {
+	        					//we found a javax.servlet.
+	        					//check if it is a javax.servlet.http
+	        					if (importstmt.contains(JAVAX_SERVLET_HTTP))
+	        						servletHttpFound = true;
+        				} else {
+        					servletFound = true;
+        				}
+        			}
+				}
+        		if (!servletFound) {
+        			importPackages += ","+JAVAX_SERVLET;
+        		}
+        		if (!servletHttpFound) {
+        			importPackages += ","+JAVAX_SERVLET_HTTP;
+        		}
+        	} else { //both are missing
+        		importPackages += ","+JAVAX_SERVLET+","+JAVAX_SERVLET_HTTP;
         	}
+        	
+        	instructions.setProperty("Import-Package", importPackages);
         }
+        
+        if( getConfiguration().getImportPaxLoggingPackages() )
+        {
+        	String importPackages = instructions.getProperty("Import-Package");
+            String provider = ";provider=paxlogging;resolution:=optional";
+            importPackages +=
+                    ",org.apache.commons.logging" + provider +
+                    ",org.apache.commons.logging.impl" + provider +
+                    ",org.apache.log4j" + provider +
+                    ",org.apache.log4j.spi" + provider +
+                    ",org.apache.log4j.xml" + provider +
+                    ",org.slf4j" + provider +
+                    ",org.slf4j.helpers" + provider +
+                    ",org.slf4j.spi" + provider;
+            
+            instructions.setProperty("Import-Package", importPackages);
+        }
+        
         // default no export packages
         if( !instructions.containsKey( "Export-Package" ) )
         {
@@ -108,5 +141,4 @@ public class WarConnection
         }
         return instructions;
     }
-
 }
--- pax-url-war/src/test/java/prg/ops4j/pax/url/webbundle/HandlerTest.java
+++ pax-url-war/src/test/java/prg/ops4j/pax/url/webbundle/HandlerTest.java
@@ -26,6 +26,7 @@ import java.util.Properties;
 import org.junit.Test;
 import org.ops4j.pax.url.war.internal.WarConnection;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -47,13 +48,75 @@ public class HandlerTest
             throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException
     {
         System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
-        URL url = new URL( "webbundle:file:foo.war?Import-Package=javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\"]&Web-ContextPath=/ct-testwar1_0" );
+        URL url = new URL( "webbundle:file:foo.war?Import-Package=javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\"&Web-ContextPath=/ct-testwar1_0" );
         WarConnection conn = (WarConnection) url.openConnection();
         Method mth = conn.getClass().getSuperclass().getDeclaredMethod("getInstructions");
         mth.setAccessible(true);
         Properties props = (Properties) mth.invoke(conn);
         assertNotNull( props );
         assertNotNull( props.getProperty( "Import-Package" ) );
+        assertEquals("javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet,javax.servlet.http", props.getProperty( "Import-Package" ));
     }
+    
+    /**
+     * Protocol handler can be used.
+     *
+     * @throws java.io.IOException - Unexpected
+     */
+    @Test
+    public void useImportServlet()
+            throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
+        System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
+        URL url = new URL( "webbundle:file:foo.war?Import-Package=javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet&Web-ContextPath=/ct-testwar1_0" );
+        WarConnection conn = (WarConnection) url.openConnection();
+        Method mth = conn.getClass().getSuperclass().getDeclaredMethod("getInstructions");
+        mth.setAccessible(true);
+        Properties props = (Properties) mth.invoke(conn);
+        assertNotNull( props );
+        assertNotNull( props.getProperty( "Import-Package" ) );
+        assertEquals("javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet,javax.servlet.http", props.getProperty( "Import-Package" ));
+    }    
+    
+    /**
+     * Protocol handler can be used.
+     *
+     * @throws java.io.IOException - Unexpected
+     */
+    @Test
+    public void useImportHttpServlet()
+            throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
+        System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
+        URL url = new URL( "webbundle:file:foo.war?Import-Package=javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet.http&Web-ContextPath=/ct-testwar1_0" );
+        WarConnection conn = (WarConnection) url.openConnection();
+        Method mth = conn.getClass().getSuperclass().getDeclaredMethod("getInstructions");
+        mth.setAccessible(true);
+        Properties props = (Properties) mth.invoke(conn);
+        assertNotNull( props );
+        assertNotNull( props.getProperty( "Import-Package" ) );
+        assertEquals("javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet.http,javax.servlet", props.getProperty( "Import-Package" ));
+    }    
 
+    /**
+     * Protocol handler can be used.
+     *
+     * @throws java.io.IOException - Unexpected
+     */
+    @Test
+    public void useVersionedImportServlet()
+            throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
+        System.setProperty( "java.protocol.handler.pkgs", "org.ops4j.pax.url" );
+        URL url = new URL( "webbundle:file:foo.war?Import-Package=javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet;version=\"2.5\"&Web-ContextPath=/ct-testwar1_0" );
+        WarConnection conn = (WarConnection) url.openConnection();
+        Method mth = conn.getClass().getSuperclass().getDeclaredMethod("getInstructions");
+        mth.setAccessible(true);
+        Properties props = (Properties) mth.invoke(conn);
+        assertNotNull( props );
+        assertNotNull( props.getProperty( "Import-Package" ) );
+        assertEquals("javax.servlet.jsp; version=\"[2.0,1000.0]\",javax.servlet.jsp.tagext; version=\"[2.0,1000.0]\",javax.servlet;version=\"2.5\",javax.servlet.http", props.getProperty( "Import-Package" ));
+    }    
+    
+    
 }
