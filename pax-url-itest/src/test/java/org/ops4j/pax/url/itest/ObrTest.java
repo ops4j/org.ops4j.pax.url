64f14fcbba300c2df433b18790be23517bec817e
Harald Wellmann <harald.wellmann@gmx.de>
2012-01-24 22:30:29 +0100

    [PAXURL-167] Integration tests for obr: handler

---
+++ pax-url-itest/src/test/java/org/ops4j/pax/url/itest/ObrTest.java
@@ -0,0 +1,80 @@
+package org.ops4j.pax.url.itest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+/**
+ * Tests the obr: protocol handler.
+ * 
+ * @author Harald Wellmann
+ */
+@RunWith(JUnit4TestRunner.class)
+public class ObrTest
+{
+    @Inject
+    private BundleContext bc;
+    
+    @Configuration( )
+    public Option[] config()
+    {
+        return options(
+            frameworkProperty("osgi.console").value("6666"),
+            
+            // OBR location
+            frameworkProperty("obr.repository.url").value("http://sigil.codecauldron.org/spring-external.obr"),
+            
+            // bundle under test and its dependencies
+            mavenBundle("org.ops4j.pax.url", "pax-url-obr").versionAsInProject(),
+            mavenBundle("org.ops4j.pax.url", "pax-url-commons").versionAsInProject(),
+            mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-lifecycle").versionAsInProject(),
+            mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-property").versionAsInProject(),
+            mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-tracker").versionAsInProject(),
+            mavenBundle("org.ops4j.base", "ops4j-base").versionAsInProject(),            
+            mavenBundle("org.apache.felix", "org.osgi.service.obr", "1.0.2"),
+            
+            // OBR RepositoryAdmin implementation
+            mavenBundle("org.apache.felix", "org.apache.felix.bundlerepository", "1.6.6"),
+            
+            junitBundles() );
+    }
+
+    
+    
+    @Test
+    public void installFromObrUrl() throws IOException, BundleException {
+        // check that obr: handler is available
+        URL url = new URL( "obr:com.springsource.org.apache.commons.lang/2.5.0" );
+        assertThat( url, is( notNullValue()) );
+        
+        // open stream of OBR resource
+        InputStream is = url.openStream();
+        assertThat( is, is( notNullValue()) );
+        
+        // install bundle from stream
+        Bundle bundle = bc.installBundle( "local", is );
+        assertThat( bundle, is(notNullValue()));
+        
+        // bundle should be active
+        assertThat( bundle.getState(), is(Bundle.ACTIVE));
+    }
+}
