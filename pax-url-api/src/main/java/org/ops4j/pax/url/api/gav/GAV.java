feac20227c378efb983239001591c797d03aafe0
Toni Menzel <toni.menzek@rebaze.com>
2012-04-11 10:27:14 +0200

    More Javadoc

--- pax-url-api/src/main/java/org/ops4j/pax/url/api/gav/GAV.java
+++ pax-url-api/src/main/java/org/ops4j/pax/url/api/gav/GAV.java
@@ -4,7 +4,12 @@
 package org.ops4j.pax.url.api.gav;
 
 /**
- *
+ * Typical GAV Pojo.
+ * In Pax URL we think of adding special smartness to version-less GAV instances.
+ * Reason for this is that versions are often not really meaningful at the place of GAV usage but better of managed in a larger
+ * context of a set of GAVs (say, in a set of Spring related artifacts, spring version is identical and also should be a supported 
+ * version. Its a different kind of operations who is setting versions to a correct value.
+ * 
  */
 public class GAV {
 
