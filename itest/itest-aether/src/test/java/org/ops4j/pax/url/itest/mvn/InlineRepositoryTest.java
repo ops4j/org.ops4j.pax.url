6970f3221a091032cec51f5cedeb55f1a5dba330
Harald Wellmann <harald.wellmann@gmx.de>
2013-12-29 14:45:33 +0100

    [PAXURL-236] removed unused import

--- itest/itest-aether/src/test/java/org/ops4j/pax/url/itest/mvn/InlineRepositoryTest.java
+++ itest/itest-aether/src/test/java/org/ops4j/pax/url/itest/mvn/InlineRepositoryTest.java
@@ -27,7 +27,6 @@ import java.util.UUID;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
