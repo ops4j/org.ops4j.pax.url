06739d3b66aeae9f8a9bf88040cc46a96a44f2e7
Nikolas Falco <nikolas.falco@finantix.com>
2018-11-27 20:14:19 +0100

    [PAXURL-286] Maven settings are ignored if there is a space in the path to the user's settings.xml
    When the settins.xml file path contain spaces during conversion to URI all spaces become %20 and when converted back to File this cause to DefaultSettingsBuilder failure when get the file that returns a void Settings instance.

---
+++ pax-url-aether/src/test/java/org/ops4j/pax/url/mvn/internal/SettingsTest.java
@@ -0,0 +1,44 @@
+package org.ops4j.pax.url.mvn.internal;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.settings.Settings;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.ops4j.pax.url.mvn.ServiceConstants;
+import org.ops4j.pax.url.mvn.internal.config.MavenConfigurationImpl;
+import org.ops4j.util.property.PropertiesPropertyResolver;
+
+public class SettingsTest {
+    @Rule
+    public TemporaryFolder fileRule = new TemporaryFolder();
+
+    @Test
+    public void settings_xml_in_path_that_contains_spaces() throws Exception {
+        File folderWithSpaces = fileRule.newFolder("path with spaces");
+        File settingsFile = new File(folderWithSpaces, "settings.xml");
+        FileUtils.copyURLToFile(getClass().getResource("/settings/settingsWithLocalRepository.xml"), settingsFile);
+
+        // simulate safeGetFile private method where spaces are replaced with %20
+        String settingsFileURL = settingsFile.toURI().toURL().toExternalForm();
+        assumeThat(settingsFileURL, containsString("%20"));
+
+        Properties props = new Properties();
+        props.put(ServiceConstants.PID + '.' + ServiceConstants.PROPERTY_SETTINGS_FILE, settingsFileURL);
+
+        PropertiesPropertyResolver propertyResolver = new PropertiesPropertyResolver(props);
+        MavenConfigurationImpl config = new MavenConfigurationImpl(propertyResolver, ServiceConstants.PID);
+        
+        Settings settings = config.getSettings();
+        assertThat("settings.xml was not parsed because its path contains spaces",
+                "repository", equalTo(settings.getLocalRepository()));
+    }
+}
